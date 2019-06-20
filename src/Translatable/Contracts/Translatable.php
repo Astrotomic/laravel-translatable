d2f9917bacf82d077cf3a5d27d0dea52a383145d
Gummibeer <dev.gummibeer@gmail.com>
2019-06-20 14:26:18 +0200

    fix php cs

--- src/Translatable/Contracts/Translatable.php
+++ src/Translatable/Contracts/Translatable.php
@@ -3,8 +3,8 @@
 namespace Astrotomic\Translatable\Contracts;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 interface Translatable
 {
