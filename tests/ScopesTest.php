07286b9a67505a95175e699ab4a1fcd3c26c1f1d
Lloople <d.lloople@icloud.com>
2019-10-07 08:59:57 +0200

    Get all translations when ordering by locale

--- tests/ScopesTest.php
+++ tests/ScopesTest.php
@@ -203,12 +203,20 @@ final class ScopesTest extends TestsBase
     public function test_orderByTranslation_sorts_by_key_asc()
     {
         $result = Country::orderByTranslation('name')->get();
-        $this->assertSame(2, $result->first()->id);
+        $this->assertSame(3, $result->first()->id);
     }
 
     public function test_orderByTranslation_sorts_by_key_desc()
     {
         $result = Country::orderByTranslation('name', 'desc')->get();
         $this->assertSame(1, $result->first()->id);
+    }
+
+
+    public function test_orderByTranslation_sorts_by_key_asc_even_if_locale_is_missing()
+    {
+        App::setLocale('ca');
+        $result = Country::orderByTranslation('name')->count();
+        $this->assertSame(4, $result);
     }
 }
