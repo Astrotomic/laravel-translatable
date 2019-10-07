4900dbd68e26bf351d97252a60baa9e74e99614f
David Llop <d.lloople@gmail.com>
2019-10-07 21:49:37 +0200

    Configure the test case class

---
+++ tests/TestCase.php
@@ -0,0 +1,26 @@
+<?php
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use Astrotomic\Translatable\TranslatableServiceProvider;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->loadMigrationsFrom([
+            '--database' => 'testing',
+            '--path' => realpath('tests/migrations')
+        ]);
+
+        $this->withFactories(realpath('tests/factories'));
+
+        app('config')->set('translatable.locales', ['el', 'en', 'fr', 'de', 'id', 'en-GB', 'en-US', 'de-DE', 'de-CH']);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [TranslatableServiceProvider::class];
+    }
+}
