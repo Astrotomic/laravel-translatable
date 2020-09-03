f8c7f27c4ba0e4c32c5e189dfdb65468b0e8470d
Reza Tayebi <reza.tayebi73@gmail.com>
2020-09-03 12:31:25 +0430

    use Translation in model

--- tests/Eloquent/VegetableTranslation.php
+++ tests/Eloquent/VegetableTranslation.php
@@ -2,10 +2,13 @@
 
 namespace Astrotomic\Translatable\Tests\Eloquent;
 
+use Astrotomic\Translation\Translation;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 
 class VegetableTranslation extends Eloquent
 {
+    use Translation;
+    
     public $timestamps = false;
 
     protected $fillable = [
