7e2db060cd7c2201c752fb723b63bc8fc0c764f1
Jannes <jannescb@me.com>
2020-09-09 10:22:10 +0200

    wip

--- tests/factories/CountryFactory.php
+++ tests/factories/CountryFactory.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 class CountryFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model
+     * The name of the factory's corresponding model.
      *
      * @var string
      */
